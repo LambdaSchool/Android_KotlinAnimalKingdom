4be4a090b4f01954d68a0a9806db60f1f730d986
Josh Halvorson <joshhalvorson16@gmail.com>
2019-03-12 13:14:40 -0700

    Add method that displays list of animals that breathe with lungs

--- src/Main.kt
+++ src/Main.kt
@@ -6,22 +6,23 @@ fun main(args: Array<String>){
         Mammal(4, "Sloth", 1804),
         Mammal(5, "Armadillo", 1758),
         Mammal(6, "Raccoon", 1758),
+        Mammal(7, "Bigfoot", 2021),
+        
         Bird(1, "Pigeon", 1837),
-
         Bird(2, "Peacock", 1821),
         Bird(3, "Toucan", 1758),
         Bird(4, "Parrot", 1824),
         Bird(5, "Swan", 1758),
-        Fish(1, "Salmon", 1758),
 
+        Fish(1, "Salmon", 1758),
         Fish(2, "Catfish", 1817),
-        Fish(3, "Perch", 1758),
-        Mammal(7, "Bigfoot", 2021)
+        Fish(3, "Perch", 1758)
     )
 
     //sortByYearNamedDesc(animalsList)
     //sortByNameAlphabetically(animalsList)
-    sortByMoveType(animalsList)
+    //sortByMoveType(animalsList)
+    listAnimalsThatBreathWithLungs(animalsList)
 }
 
 fun sortByYearNamedDesc(list: MutableList<Animal>){
@@ -49,6 +50,18 @@ fun sortByMoveType(list: MutableList<Animal>){
     }
     list.forEach{
         println(it.move())
+    }
+}
+
+fun listAnimalsThatBreathWithLungs(list: MutableList<Animal>){
+    var tempList = mutableListOf<Animal>()
+    list.forEach{
+        if(it.breath().split(" ")[3] == "lungs"){
+            tempList.add(it)
+        }
+    }
+    tempList.forEach{
+        println(it.name)
     }
 }
 
