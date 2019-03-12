d9210cedf83214f91225c288410641a89a11aa08
Josh Halvorson <joshhalvorson16@gmail.com>
2019-03-12 13:09:20 -0700

    Add method that display list sorted by how they move

--- src/AnimalClasses.kt
+++ src/AnimalClasses.kt
@@ -4,105 +4,105 @@ abstract class Animal(val id: Int, val name: String, val year: Int) {
         println("${name} consumed food")
     }
 
-    abstract fun move()
+    abstract fun move(): String
 
-    abstract fun breath()
+    abstract fun breath(): String
 
-    abstract fun reproduce()
+    abstract fun reproduce(): String
 }
 
 sealed class Move(animalName: String){
-    abstract fun performMove(animalName: String)
+    abstract fun performMove(animalName: String): String
 }
 
 class Fly(animalName: String): Move(animalName){
-    override fun performMove(animalName: String) {
-        println("$animalName flys")
+    override fun performMove(animalName: String): String {
+        return "$animalName flys"
     }
 }
 
 class Walk(animalName: String): Move(animalName){
-    override fun performMove(animalName: String) {
-        println("$animalName walks")
+    override fun performMove(animalName: String): String {
+        return "$animalName walks"
     }
 }
 
 class Swim(animalName: String): Move(animalName){
-    override fun performMove(animalName: String) {
-        println("$animalName swims")
+    override fun performMove(animalName: String): String {
+        return "$animalName swims"
     }
 }
 
 sealed class Breath(animalName: String){
-    abstract fun performBreath(animalName: String)
+    abstract fun performBreath(animalName: String): String
 }
 
 class Lungs(animalName: String): Breath(animalName){
-    override fun performBreath(animalName: String) {
-        println("$animalName breaths with lungs")
+    override fun performBreath(animalName: String): String {
+        return "$animalName breaths with lungs"
     }
 }
 
 class Gills(animalName: String): Breath(animalName){
-    override fun performBreath(animalName: String) {
-        println("$animalName breaths with gills")
+    override fun performBreath(animalName: String): String {
+        return "$animalName breaths with gills"
     }
 }
 
 sealed class Reproduce(animalName: String){
-    abstract fun performReproduction(animalName: String)
+    abstract fun performReproduction(animalName: String): String
 }
 
 class LiveBirths(animalName: String): Reproduce(animalName){
-    override fun performReproduction(animalName: String) {
-        println("$animalName reproduces with live births")
+    override fun performReproduction(animalName: String): String {
+        return "$animalName reproduces with live births"
     }
 }
 
 class Eggs(animalName: String): Reproduce(animalName){
-    override fun performReproduction(animalName: String) {
-        println("$animalName reproduces with eggs")
+    override fun performReproduction(animalName: String): String {
+        return "$animalName reproduces with eggs"
     }
 }
 
 class Mammal(id: Int, name: String, year: Int): Animal(id, name, year){
-    override fun move() {
-        Walk(name).performMove(name)
+    override fun move(): String {
+        return Walk(name).performMove(name)
     }
 
-    override fun breath() {
-        Lungs(name).performBreath(name)
+    override fun breath(): String {
+        return Lungs(name).performBreath(name)
     }
 
-    override fun reproduce() {
-        LiveBirths(name).performReproduction(name)
+    override fun reproduce(): String {
+        return LiveBirths(name).performReproduction(name)
     }
 }
 
 class Bird(id: Int, name: String, year: Int): Animal(id, name, year){
-    override fun move() {
-        Fly(name).performMove(name)
+    override fun move(): String {
+        return Fly(name).performMove(name)
     }
 
-    override fun breath() {
-        Lungs(name).performBreath(name)
+    override fun breath(): String {
+        return Lungs(name).performBreath(name)
     }
 
-    override fun reproduce() {
-        Eggs(name).performReproduction(name)
+    override fun reproduce(): String {
+        return Eggs(name).performReproduction(name)
     }
 }
 
 class Fish(id: Int, name: String, year: Int): Animal(id, name, year){
-    override fun move() {
-        Swim(name).performMove(name)
+    override fun move(): String {
+        return Swim(name).performMove(name)
     }
 
-    override fun breath() {
-        Gills(name).performBreath(name)
+    override fun breath(): String {
+        return Gills(name).performBreath(name)
     }
 
-    override fun reproduce() {
-        Eggs(name).performReproduction(name)
+    override fun reproduce(): String {
+        return Eggs(name).performReproduction(name)
     }
 }
--- src/Main.kt
+++ src/Main.kt
@@ -6,21 +6,22 @@ fun main(args: Array<String>){
         Mammal(4, "Sloth", 1804),
         Mammal(5, "Armadillo", 1758),
         Mammal(6, "Raccoon", 1758),
-        Mammal(7, "Bigfoot", 2021),
-
         Bird(1, "Pigeon", 1837),
+
         Bird(2, "Peacock", 1821),
         Bird(3, "Toucan", 1758),
         Bird(4, "Parrot", 1824),
         Bird(5, "Swan", 1758),
-
         Fish(1, "Salmon", 1758),
+
         Fish(2, "Catfish", 1817),
-        Fish(3, "Perch", 1758)
+        Fish(3, "Perch", 1758),
+        Mammal(7, "Bigfoot", 2021)
     )
 
     //sortByYearNamedDesc(animalsList)
-    sortByNameAlphabetically(animalsList)
+    //sortByNameAlphabetically(animalsList)
+    sortByMoveType(animalsList)
 }
 
 fun sortByYearNamedDesc(list: MutableList<Animal>){
@@ -39,6 +40,15 @@ fun sortByNameAlphabetically(list: MutableList<Animal>){
 
     list.forEach{
         println("${it.name}")
+    }
+}
+
+fun sortByMoveType(list: MutableList<Animal>){
+    list.sortBy {
+        it.move().split(" ")[1]
+    }
+    list.forEach{
+        println(it.move())
     }
 }
 
