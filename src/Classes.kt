e1ad745c902b42b46b8ba0d1e91ad894fb3c5508
KyleCole01 <kyle@misfitselectronics.com>
2019-05-28 16:10:08 -0400

    project complete, cleaned up

--- src/Classes.kt
+++ src/Classes.kt
@@ -12,30 +12,21 @@ abstract class Animal(
         return "$id.  The $name, which was named in $year, moves by ${moveType.move}, breathes using ${breatheType.breath}, and reproduces with ${reproduceType.reproduce}."
     }
 }
-
-
-
 //inherited classes
 class Mammal(id: Int, name: String, year: Int) :
     Animal(id, name, year, MoveType.WALK, BreatheType.LUNGS, ReproduceType.LIVE_BIRTHS)
-
 class Bird(id: Int, name: String, year: Int) :
     Animal(id, name, year, MoveType.FLY, BreatheType.LUNGS, ReproduceType.EGGS)
-
 class Fish(id: Int, name: String, year: Int) :
     Animal(id, name, year, MoveType.SWIM, BreatheType.GILLS, ReproduceType.EGGS)
-
-
 
 //enum classes
 enum class MoveType(val move: String) {
     WALK("walking"), FLY("flying"), SWIM("swimming")
 }
-
 enum class BreatheType(val breath: String) {
     LUNGS("lungs"), GILLS("gills")
 }
-
 enum class ReproduceType(val reproduce: String) {
     LIVE_BIRTHS("live births"), EGGS("eggs")
 }
--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,5 @@
 fun main(){
-  val animals = seedData()
+  val animals = getData()
      println("\n-----Animals as added to list-----\n")
      animals.forEach { println(it) }
      println("\n-----Animals sorted by year in descending order-----\n")
@@ -14,24 +14,15 @@ fun main(){
      println("\n-----Animals that breathe using lungs-----\n")
      animals.filter { (it.breatheType.breath.equals(BreatheType.LUNGS.breath)) }.forEach { println(it) }
      println("\n-----Animals that breathe using lungs and named in 1758-----\n")
-     animals.filter { (it.breatheType.breath.equals(BreatheType.LUNGS.breath) && it.year.equals(1758)) }
-          .forEach { println(it) }
+     animals.filter { (it.breatheType.breath.equals(BreatheType.LUNGS.breath) && it.year.equals(1758)) }.forEach { println(it) }
      println("\n-----Animals that lay eggs and breathe using lungs-----\n")
-     animals.filter {
-          (it.breatheType.breath.equals(BreatheType.LUNGS.breath) && it.reproduceType.reproduce.equals(
-               ReproduceType.EGGS.reproduce
-          ))
-     }
-          .forEach { println(it) }
+     animals.filter {(it.breatheType.breath.equals(BreatheType.LUNGS.breath) && it.reproduceType.reproduce.equals(ReproduceType.EGGS.reproduce))}.forEach { println(it)}
      println("\n-----Animals sorted alphabetically by name that were named in 1758-----\n")
-     animals.filter { it.year.equals(1758) }.sortedBy { it.name }
-          .forEach { println(it) }
-
-}
+     animals.filter { it.year.equals(1758) }.sortedBy { it.name }.forEach { println(it) }}
 //creating the arraylist
-fun seedData(): ArrayList<Animal> {
+fun getData(): ArrayList<Animal> {
      val animals = ArrayList<Animal>()
-
+          //mammals
      animals.add(Mammal(animals.size + 1, "Panda", 1869))
      animals.add(Mammal(animals.size + 1, "Zebra", 1778))
      animals.add(Mammal(animals.size + 1, "Koala", 1816))
@@ -39,16 +30,15 @@ fun seedData(): ArrayList<Animal> {
      animals.add(Mammal(animals.size + 1, "Armadillo", 1758))
      animals.add(Mammal(animals.size + 1, "Raccoon", 1758))
      animals.add(Mammal(animals.size + 1, "Bigfoot", 2021))
-
+          //birds
      animals.add(Bird(animals.size + 1, "Pigeon", 1837))
      animals.add(Bird(animals.size + 1, "Peacock", 1821))
      animals.add(Bird(animals.size + 1, "Toucan", 1758))
      animals.add(Bird(animals.size + 1, "Parrot", 1824))
      animals.add(Bird(animals.size + 1, "Swan", 1758))
-
+          //fish
      animals.add(Fish(animals.size + 1, "Salmon", 1758))
      animals.add(Fish(animals.size + 1, "Catfish", 1817))
      animals.add(Fish(animals.size + 1, "Perch", 1758))
-
      return animals
 }
